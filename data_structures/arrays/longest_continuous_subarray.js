d9585f24d8a77873d4937c1acc9c4bdb3fbdbed5
Drew Ferris <ddf8029@gmail.com>
2020-07-08 15:32:40 -0700

    added longest subarray algo

---
+++ data_structures/arrays/longest_continuous_subarray.js
@@ -0,0 +1,37 @@
+function longestSubarray(nums, limit) {
+    let count = 0,
+        result = 0,
+        current = [],
+        min = Number.MAX_SAFE_INTEGER,
+        max = Number.MIN_SAFE_INTEGER;
+
+    for (let i = 0; i < nums.length; i++) {
+        const val = nums[i];
+        
+        current.push(val);
+
+        if (current.length < result) continue;
+
+        min = Math.min(min, val);
+        max = Math.max(max, val);
+
+        let diff = Math.abs(min - max);
+
+        if (diff <= limit) {
+            count = current.length;
+        } else {
+            const removed = current.shift();
+
+            if (removed === min) {
+                min = Math.min(...current);
+            } else {
+                max = Math.max(...current);
+            }
+        }
+
+        result = Math.max(result, count);
+    }
+
+    return result;
+}
+
